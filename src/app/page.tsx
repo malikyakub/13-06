0f71e799966a033b523fa330ce0c7166fc2e3401
malik yakub <malikyakub@hotmail.com>
2025-06-13 17:15:02 +0300

    github link added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,29 @@ export default function Home() {
           <p className="text-gray-400">Hi all. I am</p>
           <h1 className="text-6xl font-bold">Malik Yakub</h1>
           <AnimatedTitle />
+          <p className="font-mono text-gray-500 mt-8">
+            // find my profile on Github:
+          </p>
+          <a
+            href="https://github.com/malikyakub"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-mono hover:opacity-80 transition-opacity inline-block"
+          >
+            <span className="text-purple-500">const</span>{" "}
+            <span className="text-blue-400">githubLink</span>{" "}
+            <span className="text-gray-400">= </span>{" "}
+            <span className="text-green-400">
+              <span className="hidden sm:inline">
+                "https://github.com/malikyakub"
+              </span>
+              <span className="sm:hidden">
+                "https://github.com/
+                <br />
+                malikyakub"
+              </span>
+            </span>
+          </a>
         </div>
       </div>
     </>
